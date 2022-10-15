cc8d0aade0fa5d417047ffb430c1b2127de4412d
Chris Poturalski <ChrisPoturalski@chriss-mbp.lan>
2022-10-15 12:52:28 -0700

    added the characters to be used

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Characters that will be used in the password generator
+const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const special = ["!","@","#","$","%","^","&","*","/"]
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
